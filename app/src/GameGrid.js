64a1526f3c5bf1b1bcb713a35d744b8e737605cc
ethanmchls <ethanmchls@gmail.com>
2024-02-27 15:58:33 -0700

    added border to game grid and cleaned up some variables

--- app/src/GameGrid.js
+++ app/src/GameGrid.js
@@ -1,100 +1,3 @@
-/*
-
-import React, { useState } from 'react';
-import Pawn from './Pawn';
-import { Footer } from './footer';
-import './App.css';
-
-export const GameScreen = () => {
-  var pawn1 = [0, 8];
-  var pawn2 = [16, 8];
-  var walls = ["1-0", "1-1", "1-2", "10-7", "11-7", "12-7"];
-  return (
-    <>
-    <div className='App'>
-      <GameGrid pawn1Pos={pawn1} pawn2Pos={pawn2} walls={walls} />
-      <Footer />
-    </div>
-    </>
-  );
-};
-
-const GridCell = ({ pos, classes, children }) => {
-  return (
-    <div key={`${pos}`} className={`${classes} justify-center items-center`}>
-      {children}
-    </div>
-  );
-};
-
-export const GameGrid = ({ pawn1Pos, pawn2Pos, walls }) => {
-  const gridSize = 17;
-  // Constants used to adjust the grid cell attributes
-  const cellHeight = "h-[70px]";
-  const cellWidth = "w-[70px]";
-  const wallWidth = "w-[15px]";
-  const wallHeight = "h-[15px]";
-  const emptyWallColor = "bg-gray-200";
-  const placedWallColor = "bg-black";
-  const cellColor = "bg-gray-300";
-
-  const [pawn1Clicked, setPawn1Clicked] = useState(false);
-  const [pawn2Clicked, setPawn2Clicked] = useState(false);
-  const [cells1Clicked, setCells1Clicked] = useState(false);
-  const [cells2Clicked, setCells2Clicked] = useState(false);
-
-  const renderGrid = () => {
-    const grid = [];
-    for (let i = 0; i < gridSize; i++) {
-      const row = [];
-      for (let j = 0; j < gridSize; j++) {
-        const isYWall = (i % 2 !== 0);
-        const isXWall = (j % 2 !== 0);
-        const isPlacedWall = walls.includes(`${i}-${j}`);
-        const wallColor = isPlacedWall ? `${placedWallColor}` : `${emptyWallColor}`;
-
-        if (isXWall && !isYWall) {
-          row.push(<div key={`${i}-${j}`} className={`${wallWidth} ${cellHeight} ${wallColor}`}></div>);
-        }
-        else if (isYWall && !isXWall) {
-          row.push(<div key={`${i}-${j}`} className={`${cellWidth} ${wallHeight} ${wallColor}`}></div>);
-        }
-        else if (isYWall && isXWall) {
-          row.push(<div key={`${i}-${j}`} className={`${wallWidth} ${wallHeight} ${wallColor}`}></div>);
-        }
-        else {
-          row.push(<GridCell pos={`${i}-${j}`} classes={`${cellWidth} ${cellHeight} ${cellColor}`}></GridCell>)
-        }
-      }
-
-      grid.push(<div key={i} className={`flex`}>{row}</div>);
-    }
-
-    const pawn1R = pawn1Pos[0];
-    const pawn1C = pawn1Pos[1];
-    const pawn2R = pawn2Pos[0];
-    const pawn2C = pawn2Pos[1];
-
-    grid[pawn1R].props.children[pawn1C] = GridCell({ pos: `${pawn1R}-${pawn1C}`, classes: `${cellWidth} ${cellHeight} ${cellColor}`, children: <Pawn pawnColor="bg-blue-500" /> });
-    grid[pawn2R].props.children[pawn2C] = GridCell({ pos: `${pawn2R}-${pawn2C}`, classes: `${cellWidth} ${cellHeight} ${cellColor}`, children: <Pawn pawnColor="bg-red-500" /> });
-    // TODO: make neighboring cells of pawns hoverable
-    return grid;
-  };
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-auto flex mx-auto">
-        <div className="grid grid-cols-17 grow">{renderGrid()}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default GameGrid;
-*/
-
-// /*
 import React, { useState } from 'react';
 import Pawn from './Pawn';
 import { Footer } from './footer';
@@ -107,12 +10,10 @@ export const GameScreen = () => {
   var pawn2Moves = ["16-6", "16-10", "14-8"]
   var walls = ["1-0", "1-1", "1-2", "10-7", "11-7", "12-7"];
   return (
-    <>
     <div className='App'>
       <GameGrid pawn1Pos={pawn1} pawn2Pos={pawn2} walls={walls} pawn1Moves={pawn1Moves} pawn2Moves={pawn2Moves} />
       <Footer />
     </div>
-    </>
   );
 };
 
@@ -134,13 +35,12 @@ export const GameGrid = ({ pawn1Pos, pawn2Pos, pawn1Moves, pawn2Moves, walls }) 
   // Constants used to adjust the grid cell attributes
   const cellSize = "70";
   const wallSize = "15";
-  const emptyWallColor = "bg-amber-900";
-  const placedWallColor = "bg-amber-600";
-  const borderColor = "border-amber-900";
-  const cellColor = "bg-orange-950";
-  const cellColorHl = "bg-orange-300"
-  const p1Color = "bg-red-500";
-  const p2Color = "bg-yellow-500";
+  const emptyWallColor = "amber-900";
+  const placedWallColor = "amber-600";
+  const cellColor = "orange-950";
+  const cellColorHl = "orange-300"
+  const p1Color = "red-500";
+  const p2Color = "yellow-500";
   // const p3Color = "bg-red-900";  // These can be used later when 4 player logic is added
   // const p4Color = "bg-yellow-200";
 
@@ -186,16 +86,16 @@ export const GameGrid = ({ pawn1Pos, pawn2Pos, pawn1Moves, pawn2Moves, walls }) 
 
         if (isXWall && !isYWall) {
           // row.push(<div key={`${i}-${j}`} className={`${wallWidth} ${cellHeight} ${wallColor}`}></div>);
-          row.push(<div key={`${i}-${j}`} className={`w-[${wallSize}px] h-[${cellSize}px] ${wallColor}`}></div>);
+          row.push(<div key={`${i}-${j}`} className={`w-[${wallSize}px] h-[${cellSize}px] bg-${wallColor}`}></div>);
         }
         else if (isYWall && !isXWall) {
-          row.push(<div key={`${i}-${j}`} className={`w-[${cellSize}px] h-[${wallSize}px] ${wallColor}`}></div>);
+          row.push(<div key={`${i}-${j}`} className={`w-[${cellSize}px] h-[${wallSize}px] bg-${wallColor}`}></div>);
         }
         else if (isYWall && isXWall) {
-          row.push(<div key={`${i}-${j}`} className={`w-[${wallSize}px] h-[${wallSize}px] ${wallColor}`}></div>);
+          row.push(<div key={`${i}-${j}`} className={`w-[${wallSize}px] h-[${wallSize}px] bg-${wallColor}`}></div>);
         }
         else {
-          row.push(<GridCell pos={`${i}-${j}`} classes={`w-[${cellSize}px] h-[${cellSize}px] ${cellColor}`} onClick={() => {}}></GridCell>)
+          row.push(<GridCell pos={`${i}-${j}`} classes={`w-[${cellSize}px] h-[${cellSize}px] bg-${cellColor}`} onClick={() => {}}></GridCell>)
         }
       }
 
@@ -211,41 +111,43 @@ export const GameGrid = ({ pawn1Pos, pawn2Pos, pawn1Moves, pawn2Moves, walls }) 
   const pawn2R = pawn2Pos[0];
   const pawn2C = pawn2Pos[1];
 
-  grid[pawn1R].props.children[pawn1C] = <GridCell pos={`${pawn1R}-${pawn1C}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColor}`} children={<Pawn pawnColor={`${p1Color}`} onClick={handlePawn1Click} pawnID={`${pawn1R}-${pawn1C}`} />} onClick={() => {}} />;
-  grid[pawn2R].props.children[pawn2C] = <GridCell pos={`${pawn2R}-${pawn2C}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColor}`} children={<Pawn pawnColor={`${p2Color}`} onClick={handlePawn2Click} pawnID={`${pawn2R}-${pawn2C}`} />} onClick={() => {}} />;
+  grid[pawn1R].props.children[pawn1C] = <GridCell pos={`${pawn1R}-${pawn1C}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColor}`} children={<Pawn pawnColor={`bg-${p1Color}`} onClick={handlePawn1Click} pawnID={`${pawn1R}-${pawn1C}`} />} onClick={() => {}} />;
+  grid[pawn2R].props.children[pawn2C] = <GridCell pos={`${pawn2R}-${pawn2C}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColor}`} children={<Pawn pawnColor={`bg-${p2Color}`} onClick={handlePawn2Click} pawnID={`${pawn2R}-${pawn2C}`} />} onClick={() => {}} />;
   if (pawn1Clicked) {
     for (let i = 0; i < pawn1Moves.length; i++) {
       let r = pawn1Moves[i].split('-')[0];
       let c = pawn1Moves[i].split('-')[1];
-      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColorHl}`} children={<div></div>} onClick={handle1CellsClick} />
+      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColorHl}`} children={<div></div>} onClick={handle1CellsClick} />
     }
   }
   if (pawn2Clicked) {
     for (let i = 0; i < pawn2Moves.length; i++) {
       let r = pawn2Moves[i].split('-')[0];
       let c = pawn2Moves[i].split('-')[1];
-      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColorHl}`} children={<div></div>} onClick={handle2CellsClick} />
+      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColorHl}`} children={<div></div>} onClick={handle2CellsClick} />
     }
   }
   if (cells1Clicked) {
     for (let i = 0; i < pawn1Moves.length; i++) {
       let r = pawn1Moves[i].split('-')[0];
       let c = pawn1Moves[i].split('-')[1];
-      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColor}`} children={<div></div>} onClick={() => {}} />
+      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColor}`} children={<div></div>} onClick={() => {}} />
     }
   }
   if (cells2Clicked) {
     for (let i = 0; i < pawn2Moves.length; i++) {
       let r = pawn2Moves[i].split('-')[0];
       let c = pawn2Moves[i].split('-')[1];
-      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] ${cellColor}`} children={<div></div>} onClick={() => {}} />
+      grid[r].props.children[c] = <GridCell pos={`${r}-${c}`} classes={`w-[${cellSize}px] h-${cellSize}px] bg-${cellColor}`} children={<div></div>} onClick={() => {}} />
     }
   }
 
   return (
     <div className="flex justify-center items-center h-full mx-auto my-auto">
-      <div className="w-auto flex mx-auto">
-        <div className={`grid grid-cols-17 grow`}>{grid}
+      <div className={`w-auto flex mx-auto`}>
+        <div className={`rounded-md border-${emptyWallColor} border-[${wallSize}px] my-auto`}>
+          <div className={`grid grid-cols-17 grow`}>{grid}
+          </div>
         </div>
       </div>
     </div>
@@ -253,4 +155,3 @@ export const GameGrid = ({ pawn1Pos, pawn2Pos, pawn1Moves, pawn2Moves, walls }) 
 };
 
 export default GameGrid;
-// */
